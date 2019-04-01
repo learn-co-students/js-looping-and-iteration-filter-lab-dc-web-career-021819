8099fd67b156f9b21e0df1a76290bd9025f05ad7
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-31 23:52:32 -0400

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file
+
+function findMatching(objects, target) {
+  return objects.filter(function (name) { return name.toLowerCase() === target.toLowerCase();});
+}
+
+function fuzzyMatch(objects, target) {
+  return objects.filter(function (name) {
+    return name.slice(0, target.length) === target;
+  });
+}
+
+function matchName(objects, target) {
+  return objects.filter(function (driver) {
+    return driver.name.toLowerCase() === target.toLowerCase();
+  });
+}
